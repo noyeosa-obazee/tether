4aced27db79c1baf4a90013b95134e1723f68e26
noyeosa-obazee <obazeenoyeosa@gmail.com>
2026-02-08 14:07:23 +0100

    chore: prepare for deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api";
+
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
