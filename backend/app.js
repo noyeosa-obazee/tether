37e588a62809a79598986680958362a3e7e86910
noyeosa-obazee <obazeenoyeosa@gmail.com>
2026-02-08 14:11:19 +0100

    chore: add backend folder to last commit

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,12 @@ const messageRoutes = require("./routes/messageRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 require("./config/passport")(passport);
